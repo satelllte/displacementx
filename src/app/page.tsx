e0b8f260f7a69970a205dcdfad8c749626194663
George Satellite <lunaerxs@gmail.com>
2023-10-31 22:33:55 +0200

    two sections (desktop)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 export default function Home() {
   return (
-    <div>
+    <div className='absolute inset-0 flex flex-col'>
       <header className='px-2 py-4 md:py-6'>
         <h1 className='text-center text-2xl sm:text-3xl md:text-4xl'>
           Displacement X
         </h1>
       </header>
+      <main className='flex flex-1 gap-2 px-2 pb-2'>
+        <div className='flex flex-1 items-center justify-center bg-green-900'>
+          1
+        </div>
+        <div className='flex flex-1 items-center justify-center bg-pink-900'>
+          2
+        </div>
+      </main>
     </div>
   );
 }
