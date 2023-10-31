b970e1ad2079ada9bd27fc8e9e9d4736dd3ba63c
George Satellite <lunaerxs@gmail.com>
2023-10-31 22:13:02 +0200

    simplify layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={GeistSans.variable}>
-      <body className="bg-black text-white">{children}</body>
+      <body className="bg-black text-white selection:bg-sky-800">{children}</body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <div className="before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <h1 className="text-2xl sm:text-3xl md:text-4xl px-2 py-16">Displacement X</h1>
-      </div>
-    </main>
+    <div>
+      <header className="px-2 py-4 md:py-6">
+        <h1 className="text-2xl sm:text-3xl md:text-4xl text-center">Displacement X</h1>
+      </header>
+    </div>
   )
 }
