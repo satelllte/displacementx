7cd7f99ecd5d24f343276c53ae475b71198b7aa3
George Satellite <lunaerxs@gmail.com>
2023-11-08 23:25:01 +0200

    red colors

--- src/components/ui/Switch/Switch.tsx
+++ src/components/ui/Switch/Switch.tsx
@@ -19,7 +19,7 @@ export function Switch({textOff, textOn, isOn, setIsOn}: SwitchProps) {
       <RadixSwitch.Root
         id={id}
         checked={isOn}
-        className='relative h-4 w-8 cursor-default bg-sky-700 shadow-[0_2px_10px] shadow-black outline-none focus:shadow-[0_0_0_2px] focus:shadow-sky-700/50 data-[state=checked]:bg-pink-700 focus:data-[state=checked]:shadow-pink-700/50'
+        className='relative h-4 w-8 cursor-default bg-sky-700 shadow-[0_2px_10px] shadow-black outline-none focus:shadow-[0_0_0_2px] focus:shadow-sky-700/50 data-[state=checked]:bg-red-700 focus:data-[state=checked]:shadow-red-700/50'
         onCheckedChange={setIsOn}
       >
         <RadixSwitch.Thumb className='block h-full w-4 bg-white shadow-[0_2px_2px] shadow-black transition-transform duration-100 will-change-transform data-[state=checked]:translate-x-full' />
