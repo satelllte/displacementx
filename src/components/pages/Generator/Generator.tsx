741f83f927e1f48e250f3ae9473df4e68e4ac831
George Satellite <lunaerxs@gmail.com>
2023-11-11 21:46:33 +0200

    github link

--- src/components/pages/Generator/Generator.tsx
+++ src/components/pages/Generator/Generator.tsx
@@ -1,11 +1,24 @@
+import Link from 'next/link';
+import {GitHubLogoIcon} from '@radix-ui/react-icons';
 import {CanvasSection} from './CanvasSection';
 import {SettingsSection} from './SettingsSection';
 
 export function Generator() {
   return (
     <div className='mx-auto max-w-screen-2xl'>
-      <header className='p-4'>
+      <header className='flex flex-row items-center justify-between p-4'>
         <h1 className='select-none text-2xl sm:text-3xl'>Displacement X</h1>
+        <Link
+          className='focus-visible:outline focus-visible:outline-2 focus-visible:outline-red-700'
+          href='https://github.com/satelllte/displacementx'
+          target='_blank'
+        >
+          <GitHubLogoIcon
+            aria-label='GitHub repository'
+            width={20}
+            height={20}
+          />
+        </Link>
       </header>
       <main className='flex flex-col gap-8 px-4 pb-4 md:flex-row md:gap-4'>
         <div className='relative flex-[3]'>
