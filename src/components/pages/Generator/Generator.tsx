33694102f113bcedc41c2f0c32964979b4339a7b
George Satellite <lunaerxs@gmail.com>
2023-11-14 23:43:01 +0200

    update layout

--- src/components/pages/Generator/Generator.tsx
+++ src/components/pages/Generator/Generator.tsx
@@ -26,11 +26,11 @@ export function Generator() {
           />
         </Link>
       </header>
-      <main className='flex flex-col gap-8 px-4 pb-4 md:flex-row md:gap-4'>
-        <div className='relative flex-[3]'>
+      <main className='flex flex-col gap-8 px-4 pb-4 sm:flex-row sm:gap-4'>
+        <div className='relative flex-1'>
           <CanvasSection />
         </div>
-        <div className='relative flex-[2]'>
+        <div className='relative flex-1 lg:flex-[2]'>
           <SettingsSection />
         </div>
       </main>
