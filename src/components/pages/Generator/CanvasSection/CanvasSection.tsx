e3af26d69a6864c857babaa74ab67844234959d7
George Satellite <lunaerxs@gmail.com>
2023-11-15 00:30:59 +0200

    Download: include res + datetime in file name

--- src/components/pages/Generator/CanvasSection/CanvasSection.tsx
+++ src/components/pages/Generator/CanvasSection/CanvasSection.tsx
@@ -116,7 +116,21 @@ export function CanvasSection() {
     const canvas = canvasRef.current;
     if (!canvas) return;
 
-    saveImage({canvas, fileName: 'displacementx-gen'});
+    const dateTimeString = (): string => {
+      const now = new Date();
+      const y = now.getFullYear();
+      const m = String(now.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+      const d = String(now.getDate()).padStart(2, '0');
+      const hh = String(now.getHours()).padStart(2, '0');
+      const mm = String(now.getMinutes()).padStart(2, '0');
+      const ss = String(now.getSeconds()).padStart(2, '0');
+      return `${y}-${m}-${d}-${hh}${mm}${ss}`;
+    };
+
+    saveImage({
+      canvas,
+      fileName: `DisplacementX_${width}x${height}_${dateTimeString()}`,
+    });
   };
 
   const onIs8kChange = (is8k: boolean) => {
