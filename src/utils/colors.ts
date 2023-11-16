005a07a8bf5bfe3c27a12363d9cce6bb2d76d48e
George Satellite <lunaerxs@gmail.com>
2023-11-16 17:33:41 +0200

    colors: rgbToHex

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -1,31 +1,16 @@
-/**
- * Converts RGB (0 - 255) numbers to string color in `rgb(r,g,b)` format.
- */
-export const rgb = ({r, g, b}: {r: number; g: number; b: number}): string =>
-  `rgb(${r},${g},${b})`;
+import {type ColorRGB, type ColorRGBA} from '@/types';
 
-/**
- * Converts RGB (0 - 255) + A (0 - 100) numbers to string color in `rgb(r,g,b,a)` format.
- */
-export const rgba = ({
-  r,
-  g,
-  b,
-  a,
-}: {
-  r: number;
-  g: number;
-  b: number;
-  a: number;
-}): string => `rgb(${r},${g},${b},${a / 100})`;
+export const rgb = ({r, g, b}: ColorRGB): string => `rgb(${r},${g},${b})`;
 
-/**
- * Converts X (0 - 255) number to grayscale string color in `rgb(x,x,x)` format.
- */
+export const rgba = ({r, g, b, a}: ColorRGBA): string =>
+  `rgb(${r},${g},${b},${a / 100})`;
+
+export const rgbToHex = ({r, g, b}: ColorRGB): string => {
+  const toHex = (x: number): string => x.toString(16).padStart(2, '0');
+  return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+};
+
 export const xxx = ({x}: {x: number}): string => rgb({r: x, g: x, b: x});
 
-/**
- * Converts X (0 - 255) + A (0 - 100) numbers to grayscale string color in `rgb(x,x,x,a)` format.
- */
 export const xxxa = ({x, a}: {x: number; a: number}): string =>
   rgba({r: x, g: x, b: x, a});
