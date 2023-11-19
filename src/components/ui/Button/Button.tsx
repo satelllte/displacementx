b4be98dd0bdf8a807c0d754a818cd8d31d385c56
George Satellite <lunaerxs@gmail.com>
2023-11-19 14:03:58 +0200

    Button: better focus ui

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -20,7 +20,7 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
       className={clsx(
         disabled && 'opacity-80',
         fullWidth && 'w-full',
-        'focus:ring-sky inline-flex cursor-default select-none items-center justify-center border border-white px-2 py-1 text-sm text-white hover:bg-white/20 focus:outline-none focus-visible:ring-2 active:bg-white/30',
+        'focus-visible:ring-sky focus-visible:ring-offset-sky inline-flex cursor-default select-none items-center justify-center border border-white px-2 py-1 text-sm text-white outline-none hover:bg-white/20 focus-visible:shadow-none focus-visible:ring-2 focus-visible:ring-offset-1 active:bg-white/30',
       )}
       {...rest}
     />
