e9724c94681bc6ec4a4070092383087520b97c6b
George Satellite <lunaerxs@gmail.com>
2023-11-19 20:32:23 +0200

    randomColorRGB

--- src/utils/random.test.ts
+++ src/utils/random.test.ts
@@ -1,5 +1,11 @@
 import {type SpyInstance, describe, expect, it, vi, afterAll} from 'vitest';
-import {randomBoolean, randomInteger, randomItem} from './random';
+import {
+  randomBoolean,
+  randomColorRGB,
+  randomInteger,
+  randomItem,
+} from './random';
+import {type ColorRGB} from '@/types';
 
 describe('randomBoolean', () => {
   const {mock, unmock} = useMathRandomMock();
@@ -64,6 +70,161 @@ describe('randomBoolean', () => {
         {
           "mock": 0.9,
           "result": true,
+        },
+      ]
+    `);
+  });
+});
+
+describe('randomColorRGB', () => {
+  const {mockMultiOnce, unmock} = useMathRandomMock();
+
+  afterAll(() => {
+    unmock();
+  });
+
+  it('works correctly', () => {
+    const results: Array<{
+      mocks: number[];
+      result: ColorRGB;
+    }> = [];
+
+    const max = 0.999;
+    for (let i = 0; i < max; i += 0.1) {
+      const m = Math.round(i * 100) / 100;
+      const mocks = [
+        Math.min(m, max),
+        Math.min(m + 0.05, max),
+        Math.min(m + 0.1, max),
+      ];
+      mockMultiOnce(mocks);
+      results.push({
+        mocks,
+        result: randomColorRGB(),
+      });
+    }
+
+    expect(results).toMatchInlineSnapshot(`
+      [
+        {
+          "mocks": [
+            0,
+            0.05,
+            0.1,
+          ],
+          "result": {
+            "b": 0,
+            "g": 12,
+            "r": 25,
+          },
+        },
+        {
+          "mocks": [
+            0.1,
+            0.15000000000000002,
+            0.2,
+          ],
+          "result": {
+            "b": 25,
+            "g": 38,
+            "r": 51,
+          },
+        },
+        {
+          "mocks": [
+            0.2,
+            0.25,
+            0.30000000000000004,
+          ],
+          "result": {
+            "b": 51,
+            "g": 64,
+            "r": 76,
+          },
+        },
+        {
+          "mocks": [
+            0.3,
+            0.35,
+            0.4,
+          ],
+          "result": {
+            "b": 76,
+            "g": 89,
+            "r": 102,
+          },
+        },
+        {
+          "mocks": [
+            0.4,
+            0.45,
+            0.5,
+          ],
+          "result": {
+            "b": 102,
+            "g": 115,
+            "r": 128,
+          },
+        },
+        {
+          "mocks": [
+            0.5,
+            0.55,
+            0.6,
+          ],
+          "result": {
+            "b": 128,
+            "g": 140,
+            "r": 153,
+          },
+        },
+        {
+          "mocks": [
+            0.6,
+            0.65,
+            0.7,
+          ],
+          "result": {
+            "b": 153,
+            "g": 166,
+            "r": 179,
+          },
+        },
+        {
+          "mocks": [
+            0.7,
+            0.75,
+            0.7999999999999999,
+          ],
+          "result": {
+            "b": 179,
+            "g": 192,
+            "r": 204,
+          },
+        },
+        {
+          "mocks": [
+            0.8,
+            0.8500000000000001,
+            0.9,
+          ],
+          "result": {
+            "b": 204,
+            "g": 217,
+            "r": 230,
+          },
+        },
+        {
+          "mocks": [
+            0.9,
+            0.9500000000000001,
+            0.999,
+          ],
+          "result": {
+            "b": 230,
+            "g": 243,
+            "r": 255,
+          },
         },
       ]
     `);
@@ -277,6 +438,13 @@ const useMathRandomMock = () => {
     vi.spyOn(global.Math, 'random').mockReturnValue(returnValue);
   };
 
+  const mockMultiOnce = (returnValues: number[]) => {
+    unmock();
+    returnValues.forEach((returnValue) => {
+      _spy = vi.spyOn(global.Math, 'random').mockReturnValueOnce(returnValue);
+    });
+  };
+
   const unmock = () => {
     _spy?.mockRestore();
     _spy = undefined;
@@ -284,6 +452,7 @@ const useMathRandomMock = () => {
 
   return {
     mock,
+    mockMultiOnce,
     unmock,
   };
 };
