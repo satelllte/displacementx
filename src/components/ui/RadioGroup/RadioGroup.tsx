8ccf88b18f8513da12c6ef65006cc8545c3383b6
George Satellite <lunaerxs@gmail.com>
2023-11-19 15:10:34 +0200

    RadioGroup

---
+++ src/components/ui/RadioGroup/RadioGroup.tsx
@@ -0,0 +1,56 @@
+import * as RadixRadioGroup from '@radix-ui/react-radio-group';
+import {useId} from 'react';
+
+type RadioGroupProps<T> = {
+  readonly 'aria-label': string;
+  readonly items: Array<{
+    value: T;
+    label: string;
+  }>;
+  readonly value: T;
+  readonly setValue: (value: T) => void;
+};
+
+export function RadioGroup<T extends string>({
+  'aria-label': ariaLabel,
+  items,
+  value,
+  setValue,
+}: RadioGroupProps<T>) {
+  return (
+    <RadixRadioGroup.Root
+      aria-label={ariaLabel}
+      className='flex flex-col gap-1'
+      value={value}
+      onValueChange={setValue}
+    >
+      {items.map(({value, label}) => (
+        <Item key={value} value={value} label={label} />
+      ))}
+    </RadixRadioGroup.Root>
+  );
+}
+
+function Item({
+  value,
+  label,
+}: {
+  readonly value: string;
+  readonly label: string;
+}) {
+  const id = useId();
+  return (
+    <div className='flex items-center'>
+      <RadixRadioGroup.Item
+        className='h-4 w-4 cursor-default bg-white shadow-[0_2px_10px] shadow-black outline-none hover:bg-white/80 focus:shadow-[0_0_0_2px] focus:shadow-sky'
+        value={value}
+        id={id}
+      >
+        <RadixRadioGroup.Indicator className="relative flex h-full w-full items-center justify-center after:block after:h-3 after:w-3 after:bg-pink after:content-['']" />
+      </RadixRadioGroup.Item>
+      <label className='pl-2 text-sm text-white' htmlFor={id}>
+        {label}
+      </label>
+    </div>
+  );
+}
