c7dff94f421a5c3d0804029244705b774bc7e54d
Yehor M <lunaerxs@gmail.com>
2024-02-03 15:18:45 +0200

    CI: speed-up by splitting all tests into separate commands

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  eslint: {
+    ignoreDuringBuilds: true, // Checking in "test:lint" script instead
+  },
+  typescript: {
+    ignoreBuildErrors: true, // Checking in "test:types" script instead
+  },
 };
 
 module.exports = nextConfig;
